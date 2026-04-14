6aed950bab4da47a363a97a9610b38e664ad0308
taminhquan-iot <taminhquansg@gmail.com>
2026-04-14 15:06:24 +0700

    feat: them kenh A2

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -3,9 +3,9 @@
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValues[2]; 
-float nhietDo[2]; 
-char chuoi[30];
+int adcValues[3]; 
+float nhietDo[3]; 
+char chuoi[50];
 
 void setup() {
   Serial.begin(9600);
@@ -18,8 +18,11 @@ void loop() {
   
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  adcValues[2] = analogRead(A2);
+  nhietDo[2] = (adcValues[2] * 500.0) / 1023.0;
   
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+  sprintf(chuoi, "%d,%d,%d\n", (int)nhietDo[0], (int)nhietDo[1], (int)nhietDo[2]);
   Serial.print(chuoi);
   delay(100);
 }
